73507016915dd5d0147f95efd9ffef0c4c9e3a0d
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2021-11-10 17:46:17 +0530

    Time: 4 ms (90.93%), Space: 13 MB (46.42%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& a) {
+        int n = a.size();
+        int ans = 0;
+        
+        for(int i = 1 ; i< n ; i++)
+        {
+            if(a[i] > a[i-1])
+                ans += a[i] - a[i-1];
+        }
+        return ans;
+    }
+};
