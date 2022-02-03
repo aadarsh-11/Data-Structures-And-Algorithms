9493612ae1192d9d54ef9f065a1132df2ef7854d
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-02-03 12:36:38 +0530

    Time: 484 ms (16.65%), Space: 36.7 MB (5.03%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& a, vector<int>& b, vector<int>& c, vector<int>& d){
-        map<ll,ll> mp,mp2;
+        map<ll,ll> mp,mp2,mp3;
         for(auto i:a)
         {
             mp[-i]++;
@@ -20,24 +20,27 @@ public:
         mp2.clear();
         for(auto i: c)
         {
-            for(auto x:mp)
-            {
-                mp2[x.first-i] += x.second;
-            }
+            mp3[i]++;
         }
-        mp.clear();
-        mp = mp2;
         mp2.clear();
+        mp2 = mp3;
+        mp3.clear();
         
         int ans = 0;
         
         for(auto i: d)
         {
-            if(mp[i]) 
+            for(auto x:mp2)
             {
-                ans += mp[i];
+                mp3[x.first+i] += x.second;
             }
         }
+        
+        for(auto i: mp3)
+        {
+            if(mp[i.first]) ans += i.second*mp[i.first];
+        }
+        
         return ans;
     }
 };
