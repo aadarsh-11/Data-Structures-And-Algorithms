97142c6d57119a46fc5bfb059865b6c8724f32c0
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-02-24 20:23:27 +0530

    Time: 136 ms (56.05%), Space: 30.1 MB (53.87%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
@@ -11,49 +11,26 @@
  */
 class Solution {
 public:
-    map<int, vector<TreeNode*>>dp;
-    vector<TreeNode*> allPossibleFBT(int n) {
-        if(dp.find(n) != dp.end()) return dp[n];
-        vector<TreeNode*> v;
-        if(n == 0 or n == 2)
-        {
-            return v;
-        }
-        if(n == 1)
-        {
-            TreeNode* root = new TreeNode();
-            v.push_back(root);
-            return v;
-        }
-        if(n == 3)
-        {
-            TreeNode* l = new TreeNode();            
-            TreeNode* r = new TreeNode();
-            TreeNode* root = new TreeNode(0,l,r);
-        }
-        
-        for(int i = 0; i<=n/2; i++)
-        {
-            vector<TreeNode*> lst = allPossibleFBT(i);
-            vector<TreeNode*> rst = lst;
-            if(i != n-i-1)
-                rst = allPossibleFBT(n-i-1);
-                
-            for(auto l:  lst)
-            {
-                for(auto r: rst)
-                {
-                    TreeNode* root = new TreeNode(0,l,r);
-                    v.push_back(root);
-                    
-                    if(i != n-i-1)
-                    {
-                        root = new TreeNode(0,r,l);
-                        v.push_back(root);                        
+    unordered_map<int, vector<TreeNode*>> cache;
+    vector<TreeNode*> allPossibleFBT(int N) {
+        vector<TreeNode*> res;
+        if(cache[N].size() != 0) return cache[N];
+        if(N == 1) {
+            res.push_back(new TreeNode(0));
+        } else {
+            for (int i = 1; i < N; i += 2) {
+                int l = i, r = N - i - 1;
+                for (TreeNode* left : allPossibleFBT(l)) {
+                    for (TreeNode* right : allPossibleFBT(r)) {
+                        TreeNode * root = new TreeNode(0);
+                        root->left = left;
+                        root->right = right;
+                        res.push_back(root);
                     }
                 }
             }
         }
-        return dp[n] = v;
+        cache[N] = res;
+        return res;
     }
 };
