ea0049d360fa746ae15dfd3b6d22eb975e7e57ea
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-02-28 19:16:09 +0530

    Time: 322 ms (10.21%), Space: 77.2 MB (16.31%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,49 +1,17 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& h) {
-        stack<vector<int>> st;
-        int n = h.size();
-        int ans = 0;
-        for(int i = 0 ; i < n; i++)
-        {
-            if(st.empty())
-            {
-                st.push({h[i],i});
-                ans = max(ans,h[i]);
+    int largestRectangleArea(vector<int>& height) {
+        height.push_back(0);
+        int n = height.size(), area = 0;
+        stack<int> indexes;
+        for (int i = 0; i < n; i++) {
+            while (!indexes.empty() && height[indexes.top()] > height[i]) {
+                int h = height[indexes.top()]; indexes.pop();
+                int leftLeast = indexes.empty() ? -1 : indexes.top();
+                area = max(area, h * (i - leftLeast - 1));
             }
-            else
-            {
-                int ti = i-1;
-                int lastInd = ti;
-                bool flag = 0;
-                while(!st.empty() and st.top()[0] > h[i])
-                {
-                    flag = 1;
-                    int height2 = st.top()[0];
-                    lastInd = st.top()[1];
-                    ans = max(ans, height2*(ti-st.top()[1]+1));
-                    // cout<<i<<"--"<<ans<<" "<<height2<<" "<<st.top()[1]<<endl;
-                    st.pop();
-                }
-                if(flag)
-                    st.push({h[i],lastInd});
-                else
-                    st.push({h[i],i});
-                ans = max(ans, h[i]);
-            }
-            // cout<<i<<" : "<<ans<<endl;
+            indexes.push(i);
         }
-        if(!st.empty())
-        {
-            st.push({h[n-1],n-1});
-            int ti = st.top()[1];
-            while(!st.empty())
-            {
-                int height2 = st.top()[0];
-                ans = max(ans, height2*(ti-st.top()[1]+1));
-                st.pop();
-            }
-        }
-        return ans;
+        return area; 
     }
 };
