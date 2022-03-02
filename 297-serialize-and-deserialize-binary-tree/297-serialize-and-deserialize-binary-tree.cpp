3e58421fc2992901c48d0c32c883ba9cd0109e03
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-03-02 14:41:40 +0530

    Time: 91 ms (38.09%), Space: 37.4 MB (36.62%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -12,73 +12,26 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        // base case;
         if(!root) return "#";
-        
-        // add root value;
-        string ans = to_string(root->val) + ",";
-        
-        //call for left rec
-        string left = serialize(root->left);
-        int ls = left.size()+1;
-        
-        //add size of left substring and then add left string;
-        ans += to_string(ls) + "," + left + ",";
-        
-        //do the same for right;
-        string right = serialize(root->right);
-        ans += right;
-
-        return ans;
+        return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        // check for null Node
-        if(data[0] == '#') return NULL;
+        stringstream ss(data);
+        return helper(ss);
+    }
+    
+    TreeNode* helper(stringstream &ss) {
+        string s;
+        getline(ss, s, ',');
         
-        int i = 0;
-        int sign = 0;
-        int val = 0;
+        if(s == "#")
+            return NULL;
         
-        // check for negetive sign
-        if(data[i] == '-')
-        {
-            sign = 1;
-            i++;
-        }
-        
-        //calculate root val;
-        while(data[i] != ',')
-        {
-            val = val*10 + (data[i]-'0');
-            i++;
-        }
-        if(sign) val *= -1;
-        
-        //create root node
-        TreeNode* root = new TreeNode(val);
-        
-        // currently at comma so go ahead
-        i++;
-        
-        //size of left subtree string
-        int ls = 0;
-        while(data[i] != ',')
-        {
-            ls = ls*10 + (data[i]-'0');
-            i++;
-        }
-        i++;
-        
-        //recurse for the left subtree
-        root->left = deserialize(data.substr(i,ls));
-        
-        //go to the first index of right subtree string
-        i += ls;
-        
-        // recurse for the right subtree
-        root->right = deserialize(data.substr(i, data.size()-ls-2));
+        TreeNode* root = new TreeNode(stoi(s));
+        root->left = helper(ss);
+        root->right = helper(ss);
         
         return root;
     }
