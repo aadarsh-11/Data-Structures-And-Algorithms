f4095c51cb5dab4e117d3d82fef97a674a0864cb
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-03-06 10:26:20 +0530

    Time: 5 ms (35.85%), Space: 6 MB (53.14%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int countOrders(int n) {
-        long long int dp;
+        int dp;
         dp = 1;
-        long long int mod = 1000000007;
+        int mod = 1000000007;
         for(int i =2; i<=n; i++)
         {
             long long int x = (i-1)*2+1;
             dp = (dp * ((x*(x+1))/2)) %mod;
         }
-        return (int)dp;
+        return dp;
     }
 };
