c1b0f7f9c6543e86d333cfd2fa1f9030850ae334
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-03-13 19:55:11 +0530

    Time: 309 ms (73.24%), Space: 172 MB (96.71%) - LeetHub

--- 1955-count-number-of-special-subsequences/1955-count-number-of-special-subsequences.cpp
+++ 1955-count-number-of-special-subsequences/1955-count-number-of-special-subsequences.cpp
@@ -2,39 +2,17 @@ class Solution {
 public:
     int countSpecialSubsequences(vector<int>& a) {
         int n = a.size();
-        vector<vector<int>> dp(n, vector<int>(3,0));
+        vector<int> dp(3,0);
         int mod = 1e9+7;
         for(int i = 0; i<n; i++)
         {
-            if(i == 0)
-            {
-                if(a[i] == 0)
-                dp[i][0] = 1;
-                continue;
-            }
             if(a[i] == 0)
-            {
-                if(dp[i-1][0] == 0)
-                    dp[i][0] = (dp[i-1][0]+1)%mod;
-                else
-                    dp[i][0] = ((dp[i-1][0] +1) *2 -1)%mod;
-                
-                dp[i][1] = dp[i-1][1];
-                dp[i][2] = dp[i-1][2];
-            }
+                dp[0] = (dp[0]*2 + 1)%mod;
             else if(a[i] == 1)
-            {
-                dp[i][1] = ((dp[i-1][1]*2)%mod + dp[i-1][0])%mod;
-                dp[i][0] = dp[i-1][0];
-                dp[i][2] = dp[i-1][2];
-            }
+                dp[1] = ((dp[1]*2)%mod + dp[0])%mod;
             else
-            {
-                dp[i][2] = ((dp[i-1][2] *2)%mod + dp[i-1][1])%mod;
-                dp[i][0] = dp[i-1][0];
-                dp[i][1] = dp[i-1][1];
-            }
+                dp[2] = ((dp[2] *2)%mod + dp[1])%mod;
         }
-        return dp[n-1][2];
+        return dp[2];
     }
 };
