59a655850bfd4e5df18d9f4305ba9e8fb0ed8e02
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-03-13 11:25:01 +0530

    Time: 1073 ms (6.10%), Space: 291.8 MB (11.27%) - LeetHub

---
+++ 1955-count-number-of-special-subsequences/1955-count-number-of-special-subsequences.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int countSpecialSubsequences(vector<int>& a) {
+        int n = a.size();
+        vector<vector<int>> dp(n, vector<int>(3,0));
+        int mod = 1e9+7;
+        for(int i = 0; i<n; i++)
+        {
+            if(i == 0)
+            {
+                if(a[i] == 0)
+                dp[i][0] = 1;
+                continue;
+            }
+            if(a[i] == 0)
+            {
+                if(dp[i-1][0] == 0)
+                    dp[i][0] = dp[i-1][0]+1;
+                else
+                    dp[i][0] = (dp[i-1][0] +1) *2 -1;
+                
+                dp[i][1] = dp[i-1][1];
+                dp[i][2] = dp[i-1][2];
+            }
+            else if(a[i] == 1)
+            {
+                dp[i][1] = (dp[i-1][1]*2)%mod + dp[i-1][0];
+                dp[i][0] = dp[i-1][0];
+                dp[i][2] = dp[i-1][2];
+            }
+            else
+            {
+                dp[i][2] = (dp[i-1][2] *2)%mod + dp[i-1][1];
+                dp[i][0] = dp[i-1][0];
+                dp[i][1] = dp[i-1][1];
+            }
+            for(int j = 0 ; j< 3; j++)
+            {
+                dp[i][j] %= mod;
+            }
+        }
+        return dp[n-1][2];
+    }
+};
