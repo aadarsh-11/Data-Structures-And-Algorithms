5bdf3e3ece6fc8253a7b2e91673d2e5312ec496c
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-03-22 12:31:15 +0530

    Time: 10 ms (72.45%), Space: 14.2 MB (27.94%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> vis;
-    void dfs(vector<vector<int>>& a, int i, int j, int val, int sc)
+    void dfs(vector<vector<int>>& a, int i, int j, int val, int sc, vector<vector<int>>& vis)
     {
         if(i<0 or i>= a.size() or j<0 or j>=a[0].size() or a[i][j] != sc or vis[i][j]) return;
         a[i][j] = val;
         vis[i][j] = 1;
-        dfs(a,i+1,j,val,sc);
-        dfs(a,i-1,j,val,sc);
-        dfs(a,i,j+1,val,sc);
-        dfs(a,i,j-1,val,sc);
+        dfs(a,i+1,j,val,sc,vis);
+        dfs(a,i-1,j,val,sc,vis);
+        dfs(a,i,j+1,val,sc,vis);
+        dfs(a,i,j-1,val,sc,vis);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& a, int sr, int sc, int newColor) {
-        vector<vector<int>> vis2(a.size(),vector<int>(a[0].size(),0));
-        vis = vis2;
-        dfs(a,sr,sc,newColor, a[sr][sc]);
+        vector<vector<int>> vis(a.size(),vector<int>(a[0].size(),0));
+        dfs(a,sr,sc,newColor, a[sr][sc],vis);
         return a;
     }
 };
