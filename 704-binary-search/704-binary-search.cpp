96956c1d0daa43145506816d0f017b8a642419a9
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-03-26 11:48:21 +0530

    Time: 58 ms (36.58%), Space: 27.6 MB (13.43%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -7,10 +7,10 @@ public:
         {
             int mid = l + (r-l)/2;
             if(nums[mid] == target) return mid;
-            if(nums[mid] < target)
-                l = mid+1;
-            else
+            if(nums[mid] > target)
                 r = mid-1;
+            else
+                l = mid+1;
         }
         return -1;
     }
