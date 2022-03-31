fdd3dcbb356abdfc569678010a0bfe1276b3a443
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-03-31 13:44:56 +0530

    Time: 3 ms (70.83%), Space: 7.2 MB (43.47%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        int sum = 0;
+        int max_no = 0;
+        
+        for(auto i: nums)
+        {
+            max_no = max(max_no, i);
+            sum += i;
+        }
+        
+        //binary search the answer;
+        
+        int ans = sum;
+        
+        int left = max_no;
+        int right = sum;
+        
+        while(left <= right)
+        {
+            int mid = left + (right-left)/2;
+            
+            int currsum = 0;
+            int splits = 1;
+            for(int i = 0 ; i < nums.size(); i++)
+            {
+                currsum += nums[i];
+                if(currsum > mid)
+                {
+                    splits++;
+                    currsum = nums[i];
+                }
+            }
+            if(splits <= m)
+            {
+                ans = min(ans, mid);
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
