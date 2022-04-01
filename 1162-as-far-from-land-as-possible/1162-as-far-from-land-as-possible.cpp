9ca96e5d8ea3d668f0fff9e8e4aff82055d4039f
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-04-01 16:56:17 +0530

    Time: 577 ms (5.04%), Space: 94.3 MB (5.11%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -21,6 +21,8 @@ public:
         if(q.empty() or q.size() == n*m) return -1;
         
         vector<vector<int>> dir = {{0,1},{1,0},{0,-1}, {-1,0}};
+        int ans = 0;
+        
         while(!q.empty())
         {
             int s = q.size();
@@ -36,19 +38,19 @@ public:
                     if(i<0 or i>= n or j<0 or j>=m or grid[i][j] <= grid[x[0]][x[1]] + 1)
                         continue;
                     grid[i][j] = grid[x[0]][x[1]] + 1;
+                    ans = max(ans,grid[i][j]);
                     q.push({i,j});
                 }
             }
         }
         
-        int ans = 0;
-        for(int i = 0 ; i < n; i++)
-        {
-            for(int j = 0 ; j < m; j++)
-            {
-                ans = max(ans, grid[i][j]);
-            }
-        }
+        // for(int i = 0 ; i < n; i++)
+        // {
+        //     for(int j = 0 ; j < m; j++)
+        //     {
+        //         ans = max(ans, grid[i][j]);
+        //     }
+        // }
         return ans-1;
     }
 };
