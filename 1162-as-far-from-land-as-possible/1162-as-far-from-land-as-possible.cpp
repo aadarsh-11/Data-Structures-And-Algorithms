38463d2cf162d1c1c7ecf687feaf4e2ffc900fbf
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-04-01 17:04:10 +0530

    Time: 140 ms (22.22%), Space: 19.8 MB (62.22%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -3,7 +3,7 @@ public:
     int maxDistance(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<vector<int>> q;
+        queue<pair<int,int>> q;
         for(int i = 0 ; i < n; i++)
         {
             for(int j = 0 ; j < m; j++)
@@ -20,37 +20,26 @@ public:
         }
         if(q.empty() or q.size() == n*m) return -1;
         
-        vector<vector<int>> dir = {{0,1},{1,0},{0,-1}, {-1,0}};
+        vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1}, {-1,0}};
         int ans = 0;
         
         while(!q.empty())
         {
-            int s = q.size();
-            while(s--)
+            auto x = q.front();
+            q.pop();
+            for(auto d: dir)
             {
-                auto x = q.front();
-                q.pop();
-                for(auto d: dir)
-                {
-                    int i = x[0] + d[0];
-                    int j = x[1] + d[1];
-                    
-                    if(i<0 or i>= n or j<0 or j>=m or grid[i][j] <= grid[x[0]][x[1]] + 1)
-                        continue;
-                    grid[i][j] = grid[x[0]][x[1]] + 1;
-                    ans = max(ans,grid[i][j]);
-                    q.push({i,j});
-                }
+                int i = x.first + d.first;
+                int j = x.second + d.second;
+                
+                if(i<0 or i>= n or j<0 or j>=m or grid[i][j] <= grid[x.first][x.second] + 1)
+                    continue;
+                grid[i][j] = grid[x.first][x.second] + 1;
+                ans = max(ans,grid[i][j]);
+                q.push({i,j});
             }
         }
-        
-        // for(int i = 0 ; i < n; i++)
-        // {
-        //     for(int j = 0 ; j < m; j++)
-        //     {
-        //         ans = max(ans, grid[i][j]);
-        //     }
-        // }
+
         return ans-1;
     }
 };
