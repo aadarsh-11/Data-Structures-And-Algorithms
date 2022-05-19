3a92a429df57d407582f817bcd13dd0faa1c8353
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-05-19 12:49:33 +0530

    Time: 592 ms (5.04%), Space: 111.8 MB (5.01%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    
-    bool isval(int i, int j, vector<vector<int>>&g)
+    int dfs(int i , int j, vector<vector<int>>&a, vector<vector<int>> &vis)
     {
-        if(i>=0 and i<g.size() and j>=0 and j<g[0].size()) return true;
-        return false;
-    }
-    
-    int dfs(vector<vector<int>>&dp, vector<vector<int>>&g, int i, int j)
-    {
-        if(dp[i][j]!= -1) return dp[i][j];
-        vector<pair<int,int>> dir = {{1,0},{0,1}, {-1,0}, {0,-1}};
-        int val = 0;
-        for(auto d:dir)
+        if(vis[i][j]) return vis[i][j];
+        vector<vector<int>> dir = {{1,0},{-1,0}, {0,1}, {0,-1}};
+        
+        for(auto d: dir)
         {
-            int x = i+d.first;
-            int y = j+d.second;
-            if(isval(x,y,g) and g[x][y] > g[i][j])
-            {
-                val = max(val,dfs(dp,g,x,y));
-            }
+            int ni = i+d[0];
+            int nj = j+d[1];
+            if(ni<0 or ni>=a.size() or nj<0 or nj >= a[0].size() or a[ni][nj] <= a[i][j]) continue;
+            vis[i][j] = max(vis[i][j], dfs(ni,nj,a,vis));
         }
-        return dp[i][j] = val+1;
+        vis[i][j]++;
+        return vis[i][j];
     }
     
-    int longestIncreasingPath(vector<vector<int>>& g) {
-        vector<vector<int>> dp(g.size(), vector<int>(g[0].size(),-1));
+    int longestIncreasingPath(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        
         int ans = 1;
-        for(int i = 0 ; i < g.size(); i++)
+        
+        vector<vector<int>> vis(n,vector<int>(m));
+        // ans = dfs(2,1,a,vis);
+        for(int i = 0 ; i< n ; i++)
         {
-            for(int j = 0 ; j < g[0].size(); j++)
+            for(int j = 0 ; j < m ; j++)
             {
-                ans = max(ans , dfs(dp,g,i,j));
+                if(vis[i][j] == 0)
+                {
+                    ans = max(ans, dfs(i,j,a,vis));
+                }
+                // cout<<vis[i][j]<<" ";
             }
+            // cout<<endl;
         }
         return ans;
     }
