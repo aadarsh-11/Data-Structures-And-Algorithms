7375e875e3c50391b7dd7a8d4993d2b0d5289025
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-05-23 22:50:47 +0530

    Time: 1000 ms (8.60%), Space: 102.2 MB (18.59%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -14,20 +14,22 @@ public:
         }
         vector<vector<vector<int>>> dp(a.size(), vector<vector<int>>(m+1,vector<int>(n+1,0)));
         
-        for(int i = 0 ; i< a.size(); i++)
+        for(int z = 0; z<=m; z++)
+        {
+            for(int o = 0; o<=n; o++)
+            {
+                if(a[0].first <= z and a[0].second <= o)
+                {
+                    dp[0][z][o] = 1;
+                }
+            }
+        }
+        for(int i = 1 ; i< a.size(); i++)
         {
             for(int z = 0; z<=m; z++)
             {
                 for(int o = 0; o<=n; o++)
                 {
-                    if(i == 0)
-                    {
-                        if(a[i].first <= z and a[i].second <= o)
-                        {
-                            dp[i][z][o] = 1;
-                        }
-                        continue;
-                    }
                     if(a[i].first <= z and a[i].second <= o)
                         dp[i][z][o] = max(dp[i-1][z-a[i].first][o-a[i].second]+1, dp[i-1][z][o]);
                     else
