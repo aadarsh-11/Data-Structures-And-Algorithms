d544ef1e8e03da102c1c48d3a4155ef162fc02a6
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-05-23 22:48:18 +0530

    Time: 810 ms (13.21%), Space: 102.1 MB (20.29%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,16 +1,5 @@
 class Solution {
-public:
-    int f(vector<pair<int,int>> &a, int z, int o, int i,vector<vector<vector<int>>> & dp)
-    {
-        if(i<0) return 0;
-        if(dp[i][z][o] != -1) return dp[i][z][o];
-        if(a[i].first <= z and a[i].second <= o)
-        {
-            return dp[i][z][o] = max(f(a,z-a[i].first,o-a[i].second, i-1,dp)+1, f(a,z,o,i-1,dp));
-        }
-        return dp[i][z][o] = f(a,z,o,i-1,dp);
-    }
-    
+public:    
     int findMaxForm(vector<string>& strs, int m, int n) {
         vector<pair<int,int>> a;
         for(auto i: strs)
@@ -23,7 +12,29 @@ public:
             }
             a.push_back({x-one,one});
         }
-        vector<vector<vector<int>>> dp(a.size(), vector<vector<int>>(m+1,vector<int>(n+1,-1)));
-        return f(a,m,n,a.size()-1,dp);
+        vector<vector<vector<int>>> dp(a.size(), vector<vector<int>>(m+1,vector<int>(n+1,0)));
+        
+        for(int i = 0 ; i< a.size(); i++)
+        {
+            for(int z = 0; z<=m; z++)
+            {
+                for(int o = 0; o<=n; o++)
+                {
+                    if(i == 0)
+                    {
+                        if(a[i].first <= z and a[i].second <= o)
+                        {
+                            dp[i][z][o] = 1;
+                        }
+                        continue;
+                    }
+                    if(a[i].first <= z and a[i].second <= o)
+                        dp[i][z][o] = max(dp[i-1][z-a[i].first][o-a[i].second]+1, dp[i-1][z][o]);
+                    else
+                        dp[i][z][o] = dp[i-1][z][o];
+                }
+            }
+        }
+        return dp[a.size()-1][m][n];
     }
 };
