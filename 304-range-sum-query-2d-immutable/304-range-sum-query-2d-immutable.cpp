2a9a192e0324e6728afb7c88872e8ac9da0b1ab6
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-06-03 12:16:21 +0530

    Time: 461 ms (81.00%), Space: 147.9 MB (92.98%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -2,19 +2,38 @@ class NumMatrix {
 public:
     vector<vector<int>> dp;
     NumMatrix(vector<vector<int>>& matrix) {
-        vector<vector<int>> dp(matrix.size()+1, vector<int>(matrix[0].size()+1,0));
-        for(int i = 1 ; i<= matrix.size(); i++)
+        int n = matrix.size();
+        int m = matrix[0].size();
+        dp = vector<vector<int>>(n, vector<int>(m));
+        
+        dp[0][0] = matrix[0][0];
+        for(int i = 1 ; i<m; i++)
         {
-            for(int j = 1 ; j<= matrix[0].size(); j++)
+            dp[0][i] = dp[0][i-1] + matrix[0][i];
+        }
+        
+        for(int i = 1 ; i<n; i++)
+        {
+            dp[i][0] = dp[i-1][0] + matrix[i][0];
+        }
+        
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 1; j < m; j++)
             {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i][j];
             }
         }
-        this->dp = dp;
+            
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return dp[row2+1][col2+1] - dp[row1][col2+1] - dp[row2+1][col1] + dp[row1][col1];
+        int ans = dp[row2][col2];
+        if(row1 > 0) ans -= dp[row1-1][col2];
+        if(col1 > 0) ans -= dp[row2][col1-1];
+        if(row1 > 0 and col1 > 0) ans += dp[row1-1][col1-1];
+        
+        return ans;
     }
 };
 
