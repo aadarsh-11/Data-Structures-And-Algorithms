d258879012c557f335cf619aca5f6c2724025cbe
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-06-11 12:13:14 +0530

    Time: 1104 ms (5.09%), Space: 174 MB (8.06%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        map<int,int> mp;
+        int n = nums.size();
+        int sum = 0;
+        mp[0] = 0;
+        for(int i = 0 ; i< n ; i++)
+        {
+            sum += nums[i];
+            mp[sum] = i+1;
+        }
+        
+        sum = 0;
+        int ans = -1;
+        
+        if(mp.find(x) != mp.end()) ans = mp[x];
+        
+        for(int i = n-1; i >= 0 ; i--)
+        {
+            sum += nums[i];
+            int req = x - sum;
+            if(req < 0)
+            {
+                break;
+            }
+            if(mp.find(req) != mp.end())
+            {
+                if(ans == -1)
+                    ans = mp[req] + n - i;
+                else
+                    ans = min(ans, mp[req] + n - i);
+            }
+        }
+        return ans > n ? -1 : ans;
+    }
+};
