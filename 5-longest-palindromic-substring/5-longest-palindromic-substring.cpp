7301630645f511f50f794eeced64cd1d0cf35d3b
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-06-16 18:26:50 +0530

    Time: 1829 ms (5.01%), Space: 383.6 MB (11.47%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string t = s;
-        reverse(t.begin(),t.end());
         int n = s.size();
-        pair<int,int> maxl = {0,0};
         vector<vector<int>> dp(n+1, vector<int>(n+1));
-        for(int i = 1 ; i <= n ; i++)
+        
+        int maxlen = 1;
+        int ind = 1;
+        
+        for(int i = 1; i < n+1; i++)
         {
             for(int j = 1; j < n+1; j++)
             {
-                if(s[i-1] == t[j-1])
+                if(s[i-1] == s[n-j])
                 {
-                    if(dp[i-1][j-1] > 0)
-                    {
-                        dp[i][j] = dp[i-1][j-1]+1;
-                    }
-                    else
-                    {
-                        dp[i][j] = 1;
-                    }
-                    if(dp[i][j] > dp[maxl.first][maxl.second] and i-dp[i][j] == n-j)
-                    {
-                        maxl = {i,j};
-                    }
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 }
-                // cout<<dp[i][j]<<" ";
+                if(dp[i][j] > maxlen and i-dp[i][j] == n-j)
+                {
+                    maxlen = dp[i][j];
+                    ind = i;
+                }
             }
-            // cout<<endl;
         }
-        int len = dp[maxl.first][maxl.second];
-        string ans = s.substr(maxl.first - len,len);
-        return ans;
+        
+        return s.substr(ind - maxlen, maxlen);
     }
 };
