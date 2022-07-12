dd8bf89a45485fb4d466e2bb5edc4eac951185a0
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-07-12 10:33:36 +0530

    Time: 750 ms (28.02%), Space: 10.1 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool rec(int ind, vector<int>& vis,int cs, vector<int>&a, int target, int k)
+    {
+        if(k == 1) return 1;
+        if(target == cs) return rec(0,vis,0,a,target,k-1);
+
+        for(int i = ind; i<a.size(); i++)
+        {
+            if(!vis[i] and target - a[i] >= cs)
+            {
+                vis[i] = 1;
+                if(rec(i+1,vis,cs+a[i],a,target,k)) return true;
+                vis[i] = 0;
+            }
+        }
+        return false;
+    }
+    
+    bool makesquare(vector<int>& a) {
+        int sum = accumulate(a.begin(), a.end(),0);
+        if(a.size()<4 or sum%4) return 0;
+        
+        vector<int>vis(a.size(),0);
+        
+        return rec(0,vis,0,a,sum/4, 4);
+    }
+};
