d683384b83a1d4ac195490fa48b19df7b9b8ebb0
Aadarsh Goyal <58140163+aadarsh-11@users.noreply.github.com>
2022-08-19 00:21:58 +0530

    Time: 701 ms (5.01%), Space: 78.7 MB (48.20%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        map<int,int> mp;
+        for(auto i: arr)
+        {
+            mp[i]++;
+        }
+        vector<int> v(mp.size());
+        int i = 0;
+        for(auto x: mp)
+        {
+            v[i++] = x.second;
+        }
+        
+        sort(v.rbegin(),v.rend());
+        int n = arr.size()/2;
+        
+        int ans = 0;
+        i = 0;
+        while(n > 0)
+        {
+            ans++;
+            n-=v[i++];
+        }
+        
+        return ans;
+    }
+};
